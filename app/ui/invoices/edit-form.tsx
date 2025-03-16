8ecc903787f442e9923e5b6c8d6eefa6f4403aa0
goragoraki <gorakio0126@gmail.com>
2025-03-16 20:58:14 +0900

    error handling

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -8,8 +8,9 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { Button } from '@/app/ui/button';
-import { updateInvoice } from '@/app/lib/action';
+import { State, updateInvoice } from '@/app/lib/action';
+import { useActionState } from 'react';
+import { Button } from '../button';
 
 export default function EditInvoiceForm({
   invoice,
@@ -18,9 +19,13 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+
+  const initialState: State = { message: null, errors: {} }
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id)
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState)
+
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -33,6 +38,7 @@ export default function EditInvoiceForm({
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue={invoice.customer_id}
+              aria-describedby='customerId-error'
             >
               <option value="" disabled>
                 Select a customer
@@ -44,6 +50,15 @@ export default function EditInvoiceForm({
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          <div id='customerId-error' aria-live='polite' aria-atomic='true'>
+            {state.errors?.customerId &&
+              state.errors.customerId.map((errors) => (
+                <p className='mt-2 text-red-500 text-sm'>
+                  {errors}
+                </p>
+              ))
+            }
           </div>
         </div>
 
@@ -62,9 +77,19 @@ export default function EditInvoiceForm({
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby='amount-error'
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+          </div>
+          <div id='amount-error' aria-live='polite' aria-atomic='true'>
+            {state.errors?.amount &&
+              state.errors.amount.map((errors) => (
+                <p className='text-red-500 mt-2 text-sm' key={errors}>
+                  {errors}
+                </p>
+              ))
+            }
           </div>
         </div>
 
